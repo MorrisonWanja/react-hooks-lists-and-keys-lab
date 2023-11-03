5addda220caaee98862dfb99594697ba7d1b2262
morrison <morrisonwanja@gmail.com>
2023-11-03 09:33:13 +0300

    Complete Assignment

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,12 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return <nav>{/* display an <a> tag for each link here */}
+  <a>home</a>
+  <a>about</a>
+  <a>projects</a>
+  
+  </nav>;
 }
 
 export default NavBar;
